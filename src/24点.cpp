6937bd54cde081657f042530f51847f7b4df74f6
yuqiang1029 <3755780869@qq.com>
2024-05-08 23:11:24 +0800

    point24
    point24

--- src/24点.cpp
+++ src/24点.cpp
@@ -1,21 +1,111 @@
-#include<iostream> 
+#include<iostream>
 #include<stdio.h>
 //#include<>
 using namespace std;
-int main(){
+
+
+
+#include "point24.h"
+
+int main() {
 	int a,b,c,d;
-	cin>>a>>b>>c>>d;
-	if(a>13 | b>13 | c>13 | d>13){
-		cout<<"输大了"<<endl; 
+//	cin>>a>>b>>c>>d;
+	a='9';
+	b='3';
+	c='4';
+	d='8';
+
+//	char form[16]="1+2+6/2";
+	char opa[5]="+-*/";
+	char form[16]="1+2+6/2";
+	form[0]=a;
+	form[2]=b;
+	form[4]=c;
+	form[6]=d;
+	for(int op0=0; op0<4; ++op0) {
+		for(int op1=0; op1<4; ++op1) {
+ 
+			for(int op2=0; op2<4; ++op2) {
+
+				form[1]=opa[op0];
+				form[3]=opa[op1];
+				form[5]=opa[op2];
+				tokenizer_init(form);
+				int r=expr();
+				if (r==24) {
+					cout<<form<<"="<<r<<" V"<<endl;
+				} else {
+
+					cout<<form<<"="<<r<<endl;
+				}
+			}
+		}
+
 	}
-	
-	if(a*b==24){
-		cout<<"a*b="<<a*b;
-		
-	}else if(a+b==24){
-		cout<<"a+b="<<a+b;
-	}else{
-		cout<<"凑不了"<<endl; 
-	}
+//	if(a>13 | b>13 | c>13 | d>13){
+//		cout<<"输大了"<<endl;
+//	}
+//
+//	if(a*b==24){
+//		cout<<"a*b="<<a*b;
+//
+//	}else if(a+b==24){
+//		cout<<"a+b="<<a+b;
+//	}else{
+//		cout<<"凑不了"<<endl;
+//	}
 	return 0;
 }
+
+//#include <windows.h>
+//#include <WinUser.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//
+//    BOOL opened; // 计算器是否已经成功打开的标志
+//    STARTUPINFO si ={sizeof(si)}; // 计算器启动信息
+//    PROCESS_INFORMATION pi; // 计算器进程信息
+//    HWND h1 = NULL; // 计算器窗口指针
+//    int i=0;
+//    BOOL postMsgResult;
+//
+//
+//
+//    // 打开计算器
+//    opened = CreateProcess(NULL, "calc.exe", NULL, NULL, FALSE, 0, NULL,NULL, &si, &pi);
+//    if ( !opened ) {
+//        printf("无法打开计算器，错误码 = %d\n", GetLastError());
+//        return 1;
+//    }
+//
+//    // 找到该窗口
+//    while ( h1 == NULL && i < 3 ){
+//         h1 = FindWindow(NULL, "计算器");
+//         if ( h1 == NULL ) {
+//            i ++;
+//            Sleep(1000);
+//         }
+//    }
+//    if ( h1 == NULL ) {
+//        printf("找不到已经打开的计算器，错误码 = %d\n", GetLastError());
+//        return 1;
+//    }
+//    for(i = 0; i < 3; i++){
+//        // keybd_event(0x39,0,KEYEVENTF_KEYUP,0); // FIXME
+//        // msgResult = SendMessage(h1,WM_CHAR,'4',0); // FIXME
+//        SetLastError(0);
+//       postMsgResult = PostMessage(h1,WM_CHAR,'1'+i,0);
+//        if ( i < 2 ){
+//            PostMessage(h1,WM_CHAR,'+',0);
+//        } else {
+//            PostMessage(h1,WM_CHAR,'=',0);
+//        }
+//        printf("result=%d, error=%d\n", postMsgResult, GetLastError());
+//        Sleep(1000);
+//    }
+//
+//    system("Pause");
+//    return 0;
+//}
---
+++ src/point24.h
@@ -0,0 +1,344 @@
+/* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>          // getch()
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+#ifdef UNIT_TESTING
+#include "..\unit_test\unit_test.h"
+#endif
+/* Private typedef -----------------------------------------------------------*/
+typedef enum {
+    TOKENIZER_ERROR,        // Error
+    TOKENIZER_ENDOFINPUT,   // End of input
+    TOKENIZER_NUMBER,       // number
+    TOKENIZER_PLUS,         // +
+    TOKENIZER_MINUS,        // -
+    TOKENIZER_ASTR,         // *
+    TOKENIZER_SLASH,        // /
+    TOKENIZER_LPAREN,       // (
+    TOKENIZER_RPAREN,       // )
+} token_t;
+
+//  错误代码
+typedef enum {
+    no_error = 0,           // no error
+    syntax_error,           // syntax error
+} error_t;
+
+// 错误代码与对应的消息
+typedef struct {
+	error_t error_code;
+	char *message;
+} error_table_t;
+
+/* Private define ------------------------------------------------------------*/
+#define EXPR_LEN        512 // 允许表达式长度
+#define MAX_NUM_LEN     5   // 运算数长度
+
+// 按键
+#define KEY_BACKSPACE   8
+#define KEY_DEL         127
+#define KEY_ENTER       13
+#define KEY_ESC         27
+#define KEY_EQUAL       61
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static char *curr_char = NULL;     // 表达式中当前分析到的字符
+static char *next_char = NULL;     // 表达式中下一个字符
+static token_t current_token = TOKENIZER_ERROR;
+static error_t error_code = no_error;
+
+// 错误表
+static const error_table_t error_table[] = {
+	{.error_code = no_error,            .message = "no error"},
+	{.error_code = syntax_error,        .message = "syntax error!"}
+};
+
+/* Private function prototypes -----------------------------------------------*/
+int factor(void);
+int term(void);
+int expr(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+ * \brief  获取表达式输入, 只接收数字与加减乘除运算符与括号
+ *
+ * \param  buf 用于存储表达式的缓存
+ *
+ * \return 接收的字符数
+ */
+int get_input(char *buf) {
+	int i = 0;
+	char ch;
+
+	while (i < EXPR_LEN) {
+		ch = getch();
+		// 只接收数字与运算符, 括号
+		if (ch >= '(' && ch <= '9' && ch != ',' && ch != '.') {
+			*buf = ch;
+			// putchar(*buf);
+			printf("%c", *buf);
+			buf++;
+			i++;
+		}
+		// 处理退格
+		else if (ch == KEY_BACKSPACE || ch == KEY_DEL) {
+			if (i != 0) {
+				printf("\b \b");
+				i--;
+				buf--;
+			}
+		} else if (ch == KEY_ESC)
+			exit(0);
+		// 回车, 输入完毕
+		else if (ch == KEY_ENTER || ch == KEY_EQUAL)
+			break;
+	}
+	*buf = '\0';
+
+	return i;
+}
+
+
+
+
+/**
+ * \brief  获取单字符token类型
+ *
+ * \return token类型
+ */
+token_t siglechar(void) {
+	switch (*curr_char) {
+		case '+':
+			return TOKENIZER_PLUS;
+		case '-':
+			return TOKENIZER_MINUS;
+		case '*':
+			return TOKENIZER_ASTR;
+		case '/':
+			return TOKENIZER_SLASH;
+		case '(':
+			return TOKENIZER_LPAREN;
+		case ')':
+			return TOKENIZER_RPAREN;
+		default:
+			break;
+	}
+
+	return TOKENIZER_ERROR;
+}
+
+
+
+
+/**
+ * \brief  获取一个token
+ *
+ * \return token类型
+ */
+token_t get_next_token(void) {
+	int i;
+
+	// 表达式结束
+	if (curr_char == '\0')
+		return TOKENIZER_ENDOFINPUT;
+
+	if (isdigit(*curr_char)) {
+		// 不可超过允许数字长度
+		for (i = 0; i <= MAX_NUM_LEN; i++) {
+			// 数字结束
+			if (!isdigit(*(curr_char + i))) {
+				next_char = curr_char + i;
+				return TOKENIZER_NUMBER;
+			}
+		}
+	}
+	// 分界符
+	else if (siglechar()) {
+		next_char++;
+		return siglechar();
+	}
+
+	return TOKENIZER_ERROR;
+}
+
+
+bool tokenizer_finished(void) {
+	return *curr_char == '\0' || current_token == TOKENIZER_ENDOFINPUT;
+}
+
+
+/**
+ * \brief  错误处理
+ *
+ * \param  err 错误类型
+ */
+void error(error_t err) {
+	error_code = err;
+
+	return;
+}
+
+
+/**
+ * \brief  解析下一个token
+ */
+void tokenizer_next(void) {
+	if (tokenizer_finished())
+		return;
+
+	curr_char = next_char;
+	current_token = get_next_token();
+
+	return;
+}
+
+
+
+/**
+ * \brief  得到当前token
+ */
+token_t tokenizer_token(void) {
+	return current_token;
+}
+
+
+
+/**
+ * \brief  将ASCII形式的数字转为数值
+ */
+int tokenizer_num(void) {
+	return atoi(curr_char);
+}
+
+
+
+/**
+ * \brief  匹配token
+ */
+void accept(token_t token) {
+	if (token != tokenizer_token())
+		error(syntax_error);
+
+	tokenizer_next();
+}
+
+
+
+/**
+ * \brief  取得当前因子的值, 若当前因子(类似上式中的M)是一个表达式, 进行递归求值
+ */
+int factor(void) {
+	int r;
+
+	// 当前token的类型
+	switch (tokenizer_token()) {
+			// 数字(终结符)
+		case TOKENIZER_NUMBER:
+			// 将其由ASCII转为数字值
+			r = tokenizer_num();
+			// 根据语法规则匹配当前token
+			accept(TOKENIZER_NUMBER);
+			break;
+			// 左括号
+		case TOKENIZER_LPAREN:
+			accept(TOKENIZER_LPAREN);
+			// 将括号里的值当作一个新的表达式, 递归计算(递归是从函数expr()开始的)
+			r = expr();
+			// 当括号里的表达式处理完毕后下一个token一定是右括号
+			accept(TOKENIZER_RPAREN);
+			break;
+			// 除左括号和数字之外的其它token已经被上一级处理掉了
+			// 若有其token, 一定是表达式语法错误
+		default:
+			error(syntax_error);
+	}
+
+	// 返回因子的值
+	return r;
+}
+
+
+/**
+ * \brief  求第二级优先级(乘除)表达式的值
+ */
+int term(void) {
+	int f1, f2;
+	token_t op;
+
+	// 获取左操作数(因子)
+	f1 = factor();
+	// 获取操作符
+	op = tokenizer_token();
+
+	// 操作符只能是乘或者除(同一优先级)
+	while (op == TOKENIZER_ASTR || op == TOKENIZER_SLASH) {
+		// 下一个token
+		tokenizer_next();
+		// 获取右操作数(因子)
+		f2 = factor();
+		switch ((int)op) {
+			case TOKENIZER_ASTR:
+				f1 = f1 * f2;
+				break;
+			case TOKENIZER_SLASH:
+				f1 = f1 / f2;
+				break;
+		}
+		// 上面计算完毕的这个值将做为左操作数
+		op = tokenizer_token();
+	}
+
+	return f1;
+}
+
+/**
+ * \brief  求第一级优先级(加减)表达式的值
+ */
+int expr(void) {
+	int t1, t2 = 0;
+	token_t op;
+
+	// 第一个操作数
+	t1 = term();
+	// 获取运算符
+	op = tokenizer_token();
+
+	// 操作符只能是加或者减(同一优先级)
+	while (op == TOKENIZER_PLUS || op == TOKENIZER_MINUS) {
+		// 下一个token
+		tokenizer_next();
+		// 第二个操作数
+		t2 = term();
+		switch ((int)op) {
+			case TOKENIZER_PLUS:
+				t1 = t1 + t2;
+				break;
+			case TOKENIZER_MINUS:
+				t1 = t1 - t2;
+				break;
+		}
+		op = tokenizer_token();
+	}
+
+	return t1;
+}
+
+
+
+/**
+ * \brief  词法分析器初始化
+ *
+ * \param  expr 输入的表达式字符串
+ */
+void tokenizer_init(char *expr) {
+	curr_char = next_char = expr;
+	current_token = get_next_token();
+
+	return;
+}
